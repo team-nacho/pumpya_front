c9e8c3150840f816608fa8b21670e118a554ebd3
seonghyeoncho <chosh040@naver.com>
2024-03-27 17:00:16 +0900

    project setting

---
+++ src/AppContext.tsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext({});
+
+export const AppProdiver = ({ children }: any) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  return (
+    <AppContext.Provider value={{ loading }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
