cca8beb9c48ed0a774c40fe916a0abe6ba1747d9
seonghyeoncho <chosh040@naver.com>
2024-06-11 04:12:38 +0900

    홈 화면 수정 완료

--- src/Pages/home/HomePresentation.tsx
+++ src/Pages/home/HomePresentation.tsx
@@ -11,6 +11,7 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Center,
 } from "@chakra-ui/react";
 
 interface HomePresentationProps {
@@ -25,18 +26,20 @@ interface HomePresentationProps {
 }
 
 const HomePresentation = (props: HomePresentationProps) => (
-  <Flex flexDir="column">
-    <Heading>PUMPYA!</Heading>
-    <Button onClick={props.onOpen}>Create Party!</Button>
+  <Flex flexDir="column" flex="1">
+    <Center>
+      <Heading size="2xl" >PUMPYA!</Heading>
+    </Center>
+    <Center mb="5">
+      <Text>파티원들과 함께 여행 경비를 뿜빠야!</Text>
+    </Center>
+    <Button onClick={props.onOpen}>파티 생성하기</Button>
 
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Create your Nickname</ModalHeader>
-        <ModalCloseButton />
+      <ModalContent margin="auto" ml="20px" mr="20px">
+        <ModalHeader>이름을 입력해주세요</ModalHeader>
         <ModalBody>
-          <Text>you can use your nickname!</Text>
-          <Text>but also can use random animal nickname!</Text>
           <Input
             placeholder={props.randomName}
             onChange={props.handleInputNickName}
@@ -44,9 +47,15 @@ const HomePresentation = (props: HomePresentationProps) => (
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={props.onClickCreateParty}>
-            Create!
-          </Button>
+          <Flex justifyContent="space-between" w="100%">
+            <Button colorScheme="red" onClick={props.onClose}>
+              취소  
+            </Button>
+            <Button onClick={props.onClickCreateParty}>
+              파티 생성하기
+            </Button>
+          </Flex>
+          
         </ModalFooter>
       </ModalContent>
     </Modal>
